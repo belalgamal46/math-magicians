58b941c89f8e28057d7dc404a6b17e2d4a2346d7
belalgamal46 <belalgamal00@gmail.com>
2022-11-29 14:24:33 +0200

    Fix errors

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { PureComponent } from 'react';
 import './app.css';
-import Calculator from './components/Calculator';
+import Calculator from './components/calculator/Calculator';
 
 class App extends PureComponent {
   render() {
